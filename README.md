d1306516438c6a09ce22bfffd9de83b160c8889f
Mathias Perslev <mathias@perslev.com>
2021-10-14 10:08:14 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# U-Sleep-API-Python-Bindings
-Python bindings for the U-Sleep Webserver API
+# U-Sleep API Python Bindings
+Python bindings to the U-Sleep Webserver API
+
+
+## Purpose
+An experimental/minimal implementation of Python bindings to the U-Sleep Webserver ([https://sleep.ai.ku.dk]()) API.
+
+## Authentication
+All requests to any API endpoints must include an API authentication token. Obtain your token by:
+
+1. Log in to your account at [https://sleep.ai.ku.dk/login]().
+2. Select "Account" and "Generate API Token" from the drop-down menu.
+3. Paste the API token into your script.
+
+At the time of writing the obtained token is valid for 12 hours. Once expired, a new token must be generated following the above procedure.
+
+Keep your token(s) private as they represent your identity to the server and allows others to authenticate on yout behalf.
+
+## Example
+
+```python
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("api_example")
+
+from usleep_api import USleepAPI
+
+
+if __name__ == "__main__":
+    file_path = "./my_psg.edf"        # Insert here
+    api_token = "eyJ0eXAiOiJKV1Q..."  # Insert here
+
+    # Create a sleep stager object and a new session
+    sleep_stager = USleepAPI(personal_access_token=api_token,
+                             session_name="my_session")
+
+    # See a list of valid models and set which model to use
+    logger.info("Available models: {}".format(sleep_stager.get_model_names())
+    sleep_stager.set_model('U-Sleep v1.0')
+
+    # Upload a local file (usually .edf format)
+    sleep_stager.upload_file(file_path)
+
+    # Start the prediction on two channel groups:
+    #   1: EEG Fpz-Cz + EOG horizontal
+    #   2: EEG Pz-Oz + EOG horizontal
+    # Using 30 second windows (note: U-Slep v1.0 uses 128 Hz re-sampled signals)
+    sleep_stager.predict(channel_groups=[['EEG Fpz-Cz', 'EOG horizontal'],
+                                         ['EEG Pz-Oz', 'EOG horizontal']],
+                         data_per_prediction=128*30)
+
+    # Wait for the job to finish or stream to the log output
+    # sleep_stager.stream_prediction_log()
+    success = sleep_stager.wait_for_completion()
+
+    if success:
+        # Fetch hypnogram
+        hyp = sleep_stager.get_hypnogram()
+        logger.info(hyp['hypnogram'])
+
+        # Download hypnogram file
+        sleep_stager.download_hypnogram(out_path='./hypnogram', file_type='tsv')
+    else:
+        logger.error("Prediction failed.")
+
+    # Delete session (i.e., uploaded file, prediction and logs)
+    sleep_stager.delete_session()
+```
---
+++ usleep_api.py
@@ -0,0 +1,182 @@
+import logging
+logger = logging.getLogger("USleepAPI")
+
+import os
+import time
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+class USleepAPI:
+    def __init__(self, personal_access_token, session_name=None, url="https://sleep.ai.ku.dk:443"):
+        self.url = url.rstrip("/")
+        self.requests_session = requests.sessions.Session()
+        self.session_name = session_name
+        self.token = personal_access_token
+        self.csrf_token = self.get_csrf_token()
+        self.validate_token()
+
+    def new_session(self, session_name):
+        return SleepStager(personal_access_token=self.token,
+                           url=self.url,
+                           session_name=session_name)
+
+    def validate_token(self):
+        logger.info("Validating auth token...")
+        response = self.get("/api/v1/token/validate")
+        if response.status_code != 200 or not response.content.decode("utf-8").startswith("OK"):
+            raise ConnectionRefusedError("Invalid authentication token specified.")
+
+    def get_csrf_token(self):
+        response = self.get("/login", as_json=False, log_response=False)
+        soup = BeautifulSoup(response.content, "html.parser")
+        csrf_token = soup.find('input', attrs={"id": 'csrf_token'}).get('value')
+        return csrf_token
+
+    def _add_token_to_headers(self, headers=None):
+        headers = headers or {}
+        headers['Authorization'] = f"JWT {self.token}"
+        return headers
+
+    def _add_csrf_to_headers(self, headers=None):
+        headers = headers or {}
+        headers['X-CSRFToken'] = self.csrf_token
+        return headers
+
+    def _add_session_to_params(self, params=None):
+        params = params or {}
+        params['session_name'] = f"{self.session_name}"
+        return params
+
+    @staticmethod
+    def _log_response(response, type_):
+        logger_method = logger.info if response.status_code == 200 else logger.error
+        try:
+            content = "[JSON data] " + str(response.json())[:50] + " ..."
+        except json.JSONDecodeError:
+            content = response.content.decode('utf-8')
+        logger_method(f"Server response to {type_}: {content}")
+
+    def get(self, endpoint, as_json=False, log_response=True):
+        uri = f"{self.url}/{endpoint.lstrip('/')}"
+        headers, params = {}, {}
+        if self.token:
+            headers = self._add_token_to_headers(headers)
+        if self.session_name:
+            params = self._add_session_to_params(params)
+        response = self.requests_session.get(uri, headers=headers, params=params)
+        if log_response:
+            self._log_response(response, type_="GET")
+        if as_json and response.status_code == 200:
+            return response.json()
+        else:
+            return response
+
+    def post(self, endpoint, headers=None, data=None, json=None, files=None, params=None):
+        uri = f"{self.url}/{endpoint.lstrip('/')}"
+        headers = self._add_csrf_to_headers(headers)
+        if self.token:
+            headers = self._add_token_to_headers(headers)
+        if self.session_name:
+            params = self._add_session_to_params(params)
+        response = self.requests_session.post(uri, data,
+                                              files=files,
+                                              json=json,
+                                              params=params,
+                                              headers=headers)
+        self._log_response(response, type_="POST")
+        return response
+
+    def get_model_names(self):
+        return self.get("/api/v1/get/model_names", as_json=True)['models']
+
+    def set_model(self, model_str):
+        logger.info(f"Setting model '{model_str}'")
+        model_names = self.get_model_names()
+        if model_str not in model_names:
+            err = "Invalid model, must be in {}".format(model_names)
+            logger.error(err)
+            raise ValueError(err)
+        self.post("/api/v1/sleep_stager/set_model", data={'model': model_str})
+
+    def upload_file(self, file_path):
+        logger.info(f"Uploading file at path {file_path}. Please wait.")
+        with open(file_path, "rb") as in_f:
+            return self.post("/api/v1/file/upload", files={'PSG': in_f})
+
+    def delete_file(self):
+        return self.post('/api/v1/file/delete')
+
+    def get_configuration_options(self):
+        logger.info("Getting configuration")
+        return self.get("/api/v1/get/configuration_options", as_json=True)
+
+    def get_status(self):
+        return self.get("/api/v1/get/prediction_status", as_json=True)
+
+    def get_hypnogram(self):
+        response = self.get("/api/v1/get/hypnogram")
+        if response.status_code == 200:
+            response = response.json()
+        return response
+
+    def stream_prediction_log(self, verbose=True):
+        full_log = []
+
+        def stream(delay_sec=0):
+            """ Returns True if 'stream' should be called again, False otherwise """
+            time.sleep(delay_sec)
+            response = self.get('/api/v1/get/prediction_log', log_response=False)
+            if response.status_code != 200:
+                raise ValueError(response.content)
+            response = response.json()
+            lines = response['lines'].replace("<br>", "\n")
+            if lines:
+                if verbose:
+                    print(lines)
+                full_log.append(lines)
+            return not response['finished']
+        continue_stream = stream()
+        while continue_stream:
+            continue_stream = stream(delay_sec=2)
+        return "".join(full_log)
+
+    def wait_for_completion(self):
+        logger.info("Waiting for prediction completion...")
+        self.stream_prediction_log(verbose=False)
+        return self.get_status()['label'].lower() == "completed"
+
+    def get_session_names(self):
+        return self.get('/api/v1/sleep_stager/get_session_names', as_json=True)['session_names']
+
+    def delete_session(self):
+        return self.post('/api/v1/sleep_stager/delete_session')
+
+    def delete_all_sessions(self):
+        for session in self.get_session_names():
+            self.new_session(session).delete_session()
+
+    def download_hypnogram(self, out_path, file_type='tsv'):
+        file_type = file_type.strip(".")
+        assert file_type in ("tsv", "hyp", "npy"), "Invalid file format"
+        # Download file
+        response = self.get(f'/api/v1/download/hypnogram_{file_type}', log_response=False)
+        if response.status_code == 200:
+            # Save file to disk
+            out_path = os.path.splitext(out_path)[0] + f".{file_type}"
+            logger.info(f"Saving file to {out_path}")
+            with open(out_path, "wb") as out_f:
+                out_f.write(response.content)
+        else:
+            raise ValueError(response.content)
+
+    def predict(self, channel_groups, data_per_prediction):
+        data = {'data_per_prediction': int(data_per_prediction)}
+        entry_count = 0
+        for group_idx, group in enumerate(channel_groups):
+            for channel in group:
+                data[f'channels-{entry_count}'] = channel
+                data[f'channel_group_idx-{entry_count}'] = group_idx
+                entry_count += 1
+        return self.post("/api/v1/sleep_stager/predict", data=data)
